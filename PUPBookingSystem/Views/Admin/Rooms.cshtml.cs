074a1c1059908a924650d39c47471bf26c283236
John Gavin Deposoy <gavindeposoy2375@gmail.com>
2026-01-06 10:45:00 +0800

    feat(views): implement admin dashboard views

---
+++ PUPBookingSystem/Views/Admin/Rooms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PUPBookingSystem.Views.Admin
+{
+    public class RoomsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
